671f54fdfe6e458b9e4e25237a95e6d0ed1d4638
cleverson becker <cleverson_becker@outlook.com>
2020-04-13 19:08:07 -0300

    primeiro controller e view - Department

--- SallesWebMvc/Controllers/DepartmentsController.cs
+++ SallesWebMvc/Controllers/DepartmentsController.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SallesWebMvc.Models;
+
 
 namespace SallesWebMvc.Controllers
 {
     public class DepartmentsController : Controller
     {
         public IActionResult Index()
-        {
-            return View();
+        { 
+            List<Department> list = new List<Department>();
+            list.Add(new Department { Id = 1, Name = "Eletronics" });
+            list.Add(new Department { Id = 2, Name = "Fashion" });
+            return View(list);
         }
     }
 }
--- SallesWebMvc/Models/Department.cs
+++ SallesWebMvc/Models/Department.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace SallesWebMvc.Models
+﻿namespace SallesWebMvc.Models
 {
     public class Department
     {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
